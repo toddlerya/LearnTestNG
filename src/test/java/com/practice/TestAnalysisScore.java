bc3ca724953e2cea0bf7ec2cdba88f3eb6e86e72
toddlerya <toddlerya@gmail.com>
2020-02-22 01:18:50 +0800

    编写测试用例

---
+++ src/test/java/com/practice/TestAnalysisScore.java
@@ -0,0 +1,75 @@
+package com.practice;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ * @author evi1
+ * @date 2020/2/21 01:04
+ * ===== 分析下功能点 ======
+ * AnalysisScore类有以下方法：
+ * 1. setStudentList()   加载学生信息到列表，等待使用，不允许有重复的学号
+ * 2. getStudentList()   获取已经加载的学生信息数组
+ * 3. clearStudentList() 清空加载的学生信息数组，等待加载新的数据
+ * 4. scoreRank()        对学生成绩进行从高到低排序，成绩相同的按照学号从小到大排序
+ * 5. calcAvgScore()     计算所有学生的成绩的平均值
+ * <p>
+ * ====== 测试用例编写思路 ======
+ * 1. 在所有用例执行前创建一个AnalysisScore类的对象，后续反复使用
+ * 2. 每个方法执行完成后清理测试数据
+ */
+
+public class TestAnalysisScore {
+
+    AnalysisScore asObj = new AnalysisScore();
+
+    @BeforeMethod
+    public void clearStudentList() {
+        // 每个测试运行后清理数组，避免数据干扰
+        asObj.clearStudentList();
+    }
+
+    @Test
+    public void testSetStudentList() {
+        /*
+        测试载入学生数据是否成功
+         */
+        StudentDetails a = new StudentDetails("小王", 1, 93);
+        StudentDetails b = new StudentDetails("小刘", 2, 90);
+        asObj.setStudentList(a, b);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class,
+            expectedExceptionsMessageRegExp = "有学生学号重复.*\\s*.*\\s*.*\\s*.*",
+            groups = {"functest"})
+    public void testSetStudentListOfCheckDuplicateCode() {
+        /*
+         测试是否会校验重复学号，能否正确抛出报错信息
+         */
+        StudentDetails a = new StudentDetails("小王", 1, 93);
+        StudentDetails b = new StudentDetails("小刘", 1, 90);
+        asObj.setStudentList(a, b);
+    }
+
+    @Test
+    public void testGetStudentList() {
+
+    }
+
+    @Test
+    public void testClearStudentList() {
+        // 载入数据后，测试能否清空数据
+        asObj.clearStudentList();
+        Assert.assertNull(asObj.getStudentList(), "StudentList数组未清空");
+    }
+
+    @Test
+    public void testScoreRank() {
+    }
+
+    @Test
+    public void testCalcAvgScore() {
+    }
+}
